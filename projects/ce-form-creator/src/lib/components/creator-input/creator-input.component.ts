3cf594e7bc62b9e74cf309c5ec873b98eee112fc
fabien <fabien.pelisson@codeffekt.com>
2024-10-07 09:26:21 +0200

    Meta fields

--- projects/ce-form-creator/src/lib/components/creator-input/creator-input.component.ts
+++ projects/ce-form-creator/src/lib/components/creator-input/creator-input.component.ts
@@ -1,36 +1,115 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, forwardRef, Input, OnDestroy, Optional, Self } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { A11yModule } from '@angular/cdk/a11y';
-import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { ControlValueAccessor, NgControl, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldControl, MatFormFieldModule } from '@angular/material/form-field';
+import { Subject } from 'rxjs';
+import { BooleanInput, coerceBooleanProperty } from '@angular/cdk/coercion';
 
 @Component({
   selector: 'ce-creator-input',
   standalone: true,
   imports: [
-    CommonModule,   
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    forwardRef(() => CreatorInputComponent),
     A11yModule,
   ],
   templateUrl: './creator-input.component.html',
   styleUrls: ['./creator-input.component.scss'],
+  host: {
+    '[class.floating]': 'shouldLabelFloat',
+    '[id]': 'id',
+  },
   providers: [{
-    provide: NG_VALUE_ACCESSOR,
-    multi: true,
-    useExisting: CreatorInputComponent,
+    provide: MatFormFieldControl,
+    useExisting: CreatorInputComponent
   }]
 })
-export class CreatorInputComponent implements ControlValueAccessor {  
+export class CreatorInputComponent implements
+  ControlValueAccessor,
+  MatFormFieldControl<string>,
+  OnDestroy {
 
-  onChange = (value?: string) => { };
+  static nextId = 0;
 
+  onChange = (value: string | null) => { };
   onTouched = () => { };
 
+  focused = false;
   touched = false;
 
-  disabled = false;
-
-  value?: string = undefined;
+  _value: string | null = null;
 
   isEditMode = false;
+  invalid = false;
+
+  stateChanges = new Subject<void>;
+  controlType = 'ce-creator-input';
+  readonly id = `ce-creator-input-${CreatorInputComponent.nextId++}`;
+
+  @Input('aria-describedby') userAriaDescribedBy!: string;
+
+  @Input()
+  get placeholder(): string {
+    return this._placeholder;
+  }
+  set placeholder(value: string) {
+    this._placeholder = value;
+    this.stateChanges.next();
+  }
+  private _placeholder!: string;
+
+  @Input()
+  get value(): string | null {
+    return this._value;
+  }
+  set value(v: string | null) {
+    this._value = v;
+    this.stateChanges.next();
+  }
+
+  @Input()
+  get required(): boolean {
+    return this._required;
+  }
+  set required(value: BooleanInput) {
+    this._required = coerceBooleanProperty(value);
+    this.stateChanges.next();
+  }
+  private _required = false;
+
+  @Input()
+  get disabled(): boolean {
+    return this._disabled;
+  }
+  set disabled(value: BooleanInput) {
+    this._disabled = coerceBooleanProperty(value);
+    this.stateChanges.next();
+  }
+  private _disabled = false;
+
+  get empty() {
+    return !this._value || this._value === "";
+  }
+
+  get shouldLabelFloat() {
+    return this.focused || !this.empty;
+  }
+
+  get errorState(): boolean {
+    return this.invalid && this.touched;
+  }
+
+  constructor(
+    private _elementRef: ElementRef<HTMLElement>,
+    @Optional() @Self() public ngControl: NgControl,
+  ) {
+    if (this.ngControl != null) {
+      this.ngControl.valueAccessor = this;
+    }
+  }
 
   updateValue(newValue: string) {
     this.markAsTouched();
@@ -38,7 +117,7 @@ export class CreatorInputComponent implements ControlValueAccessor {
       this.value = newValue;
       this.onChange(this.value);
     }
-  }  
+  }
 
   writeValue(value: string) {
     this.value = value;
@@ -61,5 +140,17 @@ export class CreatorInputComponent implements ControlValueAccessor {
 
   setDisabledState(disabled: boolean) {
     this.disabled = disabled;
+  }
+
+  ngOnDestroy(): void {
+    this.stateChanges.complete();
+  }
+
+  setDescribedByIds(ids: string[]) {    
+  }
+
+  onContainerClick(event: MouseEvent) {
+    if ((event.target as Element).tagName.toLowerCase() != 'input') {      
+    }
   }
 }
--- projects/ce-form-creator/src/lib/components/dialogs/fields-dialog/fields-dialog.component.ts
+++ projects/ce-form-creator/src/lib/components/dialogs/fields-dialog/fields-dialog.component.ts
@@ -3,9 +3,15 @@ import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MAT_DIALOG_DATA, MatDialog, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
 import { MatListModule } from '@angular/material/list';
+import { CeTabsModule } from '../../layout/tabs';
 
 export interface FieldsDialogConfig {
   fields: string[];
+}
+
+interface MetaFieldItem {
+  name: string;
+  value: string;
 }
 
 @Component({
@@ -16,15 +22,39 @@ export interface FieldsDialogConfig {
     MatDialogModule,
     MatButtonModule,
     MatListModule,
+    CeTabsModule,
   ],
   templateUrl: './fields-dialog.component.html',
   styleUrls: ['./fields-dialog.component.scss']
 })
 export class FieldsDialogComponent {
-
+  
   static open(dialog: MatDialog, config: FieldsDialogConfig) { 
     return dialog.open(FieldsDialogComponent, { data: config });
   }
+
+  metaFields: MetaFieldItem[] = [
+    {
+      name: 'Id',
+      value: '$id'
+    },
+    {
+      name: 'Création date',
+      value: '$ctime'
+    },
+    {
+      name: 'Modification date',
+      value: '$mtime'
+    },
+    {
+      name: 'Author',
+      value: '$author'
+    },
+    {
+      name: 'Title',
+      value: '$title'
+    }
+  ];
 
   constructor(
     @Inject(MAT_DIALOG_DATA) public config: FieldsDialogConfig,
